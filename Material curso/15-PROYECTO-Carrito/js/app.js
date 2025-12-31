a556c7e2beafd9b105e3bf519f6b75dcc699a5f9
Nadbort ASUS Classic <narvaez.diego.co@gmail.com>
2025-12-31 14:07:51 -0500

    He creado el objeto con los datos de producto

---
+++ Material curso/15-PROYECTO-Carrito/js/app.js
@@ -0,0 +1,52 @@
+//Variables 
+const carrito = document.querySelector('#carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarritoBtn = document.querySelector('#vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+cargarEventListeners();
+function cargarEventListeners() {
+    
+    //Cuando agregas un curso presionando el boton 'Agregar al carrito'
+    listaCursos.addEventListener('click', agregarCurso);
+
+}
+
+//Funciones
+
+function agregarCurso(e) {
+
+    e.preventDefault();
+
+    if( e.target.classList.contains('agregar-carrito') ){
+
+        const cursoSeleccionado = e.target.parentElement.parentElement;
+
+        leerDatosCurso(cursoSeleccionado);
+
+    }
+}
+
+//Lee el contenido del HTML al que le dimos click y extrae la informacion del curso
+
+function leerDatosCurso(curso) {
+  
+
+    //console.log(curso);
+
+    //Crear un objeto con el contenido del curso actual
+    const infoCurso = {
+       
+        imagen: curso.querySelector('img').src,
+        titulo: curso.querySelector('h4').textContent,
+        precio: curso.querySelector('.precio span').textContent,
+        id: curso.querySelector('a').getAttribute('data-id'),
+        cantidad: 1
+
+
+
+    }
+
+    console.log(infoCurso);
+
+}
